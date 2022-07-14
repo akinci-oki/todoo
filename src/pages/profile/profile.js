32c0849acb0ace7c8f543ebc1a9b44fc6cefbe8b
Okan <okanakinci000@gmail.com>
2022-07-14 17:41:24 +0300

    post-users

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -3,6 +3,8 @@ import axios from "axios";
 
 function Profile() {
     const [users, setUsers] = useState([]);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
 
     useEffect(() => {
         getUsers();
@@ -17,32 +19,86 @@ function Profile() {
             console.error(error);
         }
     }
+    async function onAddUser() {
+        try {
+            await axios.post("http://localhost:4000/api/users", {
+                name: name,
+                email: email,
+                statistics: {
+                    registrationDate: "05.07.2022",
+                    totalOnlineTime: 5,
+                },
+                todos: ["string"],
+            });
+            getUsers();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <div>
-            <ul>
-                <div>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th> ID </th>
-                                <th> Email </th>
-                                <th> Name </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {users.map((user, index) => {
-                                return (
-                                    <tr key={index}>
-                                        <td> {user.id} </td>
-                                        <td> {user.email} </td>
-                                        <td> {user.name} </td>
-                                    </tr>
-                                );
-                            })}
-                        </tbody>
-                    </table>
+            <form>
+                <div className="input-container">
+                    <label> name </label>
+                    <input
+                        placeholder="your name"
+                        type="text"
+                        id="desc"
+                        onChange={(e) => {
+                            e.preventDefault();
+                            setName(e.target.value);
+                        }}
+                    />
                 </div>
-            </ul>
+                <div className="input-container">
+                    <label> email </label>
+                    <input
+                        placeholder="your email"
+                        type="text"
+                        id="desc"
+                        onChange={(e) => {
+                            e.preventDefault();
+                            setEmail(e.target.value);
+                        }}
+                    />
+                </div>
+
+                <div>
+                    <button
+                        className="primary"
+                        type="submit"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            onAddUser(e);
+                        }}
+                    >
+                        add
+                    </button>
+                </div>
+            </form>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th> ID </th>
+                            <th> Email </th>
+                            <th> Name </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((user, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td> {user.id} </td>
+                                    <td> {user.email} </td>
+                                    <td> {user.name} </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
