3f4f848ef4e62f118642433df6fbb09ce4b7e696
Okan <okanakinci000@gmail.com>
2022-08-01 12:01:57 +0300

    todo-1-clean-up-menu

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import Homepage from "./home/homepage";
 import Settingspage from "./settings/settingspage";
 import Profile from "./profile/profile";
-import Api from "./api/api";
 
-export { Homepage, Settingspage, Profile, Api };
+export { Homepage, Settingspage, Profile };
