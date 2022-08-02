b0d79d6f2edd15c6e5b88efe058a9f7f5ab668e7
Celine Longeville <celine.longeville@hotmail.com>
2022-08-02 17:33:47 +0200

    TODO-21 router implementation clean-up

--- src/Router.js
+++ src/Router.js
@@ -1,21 +1,16 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-import { NotFound, Menu } from "./components";
+import { NotFound } from "./components";
 import { Homepage, Settingspage, Profile } from "./pages";
 import "./App.scss";
 
 const Router = () => (
-    <BrowserRouter>
-        <div>
-            <Menu />
-            <Routes>
-                <Route exact path="/" element={<Homepage />}></Route>
-                <Route path="/settings" element={<Settingspage />}></Route>
-                <Route path="/profile" element={<Profile />}></Route>
-                <Route path="/*" element={<NotFound />}></Route>
-            </Routes>
-        </div>
-    </BrowserRouter>
+    <Routes>
+        <Route exact path="/" element={<Homepage />}></Route>
+        <Route path="/settings" element={<Settingspage />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/*" element={<NotFound />}></Route>
+    </Routes>
 );
 export default Router;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Router from "./Router";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(<Router />, document.querySelector("#root"));
+import App from "./App";
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.querySelector("#root")
+);
