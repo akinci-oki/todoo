68b3f878500dab1a83e230d19b98641ec91077e1
Okan <okanakinci000@gmail.com>
2022-08-10 16:18:29 +0300

    TODO-12-login-flow

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,26 +1,46 @@
 import { Link } from "react-router-dom";
+import { useUser } from "../../context";
 
 function Profile() {
+    const { user, setUser } = useUser();
+
     return (
         <div className="profile">
             <h2> Profile </h2>
-            <div>
-                <p>Want to be able to save your TO DO’s?</p>
-                <p>
-                    Log in with your e-mail and pick up right where you left
-                    off!
-                </p>
-            </div>
-            <div>
-                <button>
-                    <Link to="/log-in">log in</Link>
-                </button>
-            </div>
-            <div>
-                <button>
-                    <Link to="/sign-up">sign up</Link>
-                </button>
-            </div>
+            {user.firstName && (
+                <>
+                    <p>You are currently logged in as</p>
+                    <p>
+                        <strong>
+                            {user.firstName}&nbsp;
+                            {user.lastName} ({user.email})
+                        </strong>
+                    </p>
+                    <button> log out </button>
+                </>
+            )}
+
+            {!user.firstName && (
+                <>
+                    <div>
+                        <p>Want to be able to save your TO DO’s?</p>
+                        <p>
+                            Log in with your e-mail and pick up right where you
+                            left off!
+                        </p>
+                    </div>
+                    <div>
+                        <button>
+                            <Link to="/log-in">log in</Link>
+                        </button>
+                    </div>
+                    <div>
+                        <button>
+                            <Link to="/sign-up">sign up</Link>
+                        </button>
+                    </div>
+                </>
+            )}
         </div>
     );
 }
--- src/context.js
+++ src/context.js
@@ -1,7 +1,7 @@
 import { createContext, useContext } from "react";
 
 export const userContext = createContext({
-    user: "null",
-    // setUser: (user) => console.warn("No userContext provider."),
+    user: { id: "", name: "", email: "" },
+    setUser: (user) => console.warn("No userContext provider."),
 });
 export const useUser = () => useContext(userContext);
