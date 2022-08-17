178d76ebd1817ff7977372cb45610ba4fe559b7a
Okan <okanakinci000@gmail.com>
2022-08-17 16:01:27 +0300

    TODO-36-h2-margin

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function NotFound() {
     return (
         <div className="NotFound">
-            <h1>404</h1>
+            <h2>404</h2>
             <p>Uh-oh... seems like you’re lost. We couldn’t find that page.</p>
 
             <div>
