f45fb1bc93e9ed7a337bbbaaeada3739233b264b
Okan <okanakinci000@gmail.com>
2022-08-25 17:31:07 +0300

    TODO-32-update-user-wip

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,40 +1,82 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 import { useUser } from "../../context";
+import { Spinner } from "../../components";
 
 function UpdateUser() {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
     // const [isUpdated, setIsUpdated] = useState(false);
-    const [isDisabled, setIsDisabled] = useState(true);
-    // const [error, setError] = useState({
-    //     email: null,
-    // });
-    const { user } = useUser();
+    const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+    const [error, setError] = useState({
+        firstName: null,
+        lastName: null,
+        email: null,
+    });
+
+    const { user, setUser } = useUser();
 
     useEffect(() => {
         setFirstName(user.firstName);
         setLastName(user.lastName);
         setEmail(user.email);
-        setIsDisabled(true);
+        setIsButtonDisabled(true);
     }, [user]);
 
     useEffect(() => {
-        onUpdate();
+        checkIfDataChange();
     }, [firstName, lastName, email]);
 
-    const onUpdate = () => {
+    async function onUpdateUser() {
+        if (firstName.length < 1) {
+            setError((error) => ({
+                ...error,
+                firstName: "please fill in your first name.",
+            }));
+        }
+        if (lastName.length < 1) {
+            setError((error) => ({
+                ...error,
+                lastName: "please fill in your last name.",
+            }));
+        }
+        if (email.length < 1) {
+            setError((error) => ({
+                ...error,
+                email: "please fill in your email.",
+            }));
+        }
+        if (firstName.length < 1 || lastName.length < 1 || email.length < 1) {
+            return;
+            setIsLoading(true);
+        }
+        try {
+            await axios.put(`http://localhost:4000/api/users/${user.id}`, {
+                firstName,
+                lastName,
+                email,
+            });
+            setIsLoading(false);
+        } catch (error) {
+            console.error(error);
+            setIsLoading(false);
+        }
+    }
+
+    function checkIfDataChange() {
         if (user.firstName !== firstName) {
-            setIsDisabled(false);
+            setIsButtonDisabled(false);
         }
         if (user.lastName !== lastName) {
-            setIsDisabled(false);
+            setIsButtonDisabled(false);
         }
         if (user.email !== email) {
-            setIsDisabled(false);
+            setIsButtonDisabled(false);
         }
-    };
+    }
 
     return (
         <div className="update-user">
@@ -44,6 +86,7 @@ function UpdateUser() {
                     <label> first name </label>
                     <input
                         value={firstName}
+                        // disabled={isDisabled || isLoading}
                         type="text"
                         id="firstname"
                         onChange={(e) => {
@@ -51,11 +94,15 @@ function UpdateUser() {
                             setFirstName(e.target.value);
                         }}
                     />
+                    {error.firstName && (
+                        <p className="error">{error.firstName}</p>
+                    )}
                 </div>
                 <div className="input-container">
                     <label> last name </label>
                     <input
                         value={lastName}
+                        // disabled={isDisabled || isLoading}
                         type="text"
                         id="lastname"
                         onChange={(e) => {
@@ -63,11 +110,15 @@ function UpdateUser() {
                             setLastName(e.target.value);
                         }}
                     />
+                    {error.lastName && (
+                        <p className="error">{error.lastName}</p>
+                    )}
                 </div>
                 <div className="input-container">
                     <label> e-mail </label>
                     <input
                         value={email}
+                        // disabled={isDisabled || isLoading}
                         type="text"
                         id="email"
                         onChange={(e) => {
@@ -75,14 +126,19 @@ function UpdateUser() {
                             setEmail(e.target.value);
                         }}
                     />
+                    {error.email && <p className="error">{error.email}</p>}
                 </div>
                 <div>
                     <button
-                        disabled={isDisabled}
+                        disabled={isButtonDisabled || isLoading}
                         className="primary"
                         type="submit"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            onUpdateUser(e);
+                        }}
                     >
-                        update
+                        {isLoading ? <Spinner /> : "update"}
                     </button>
                 </div>
             </form>
